286e525928f6292f320583fe20e6c1169c77942c
K.VAMSHIKRISHNA <k.vamshikrishna2025@gmail.com>
2022-11-29 16:54:57 +0530

    first

--- SpringWeb/src/main/java/com/krishna/core/ProductRepository.java
+++ SpringWeb/src/main/java/com/krishna/core/ProductRepository.java
@@ -1,7 +1,6 @@
 package com.krishna.core;
 
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
-
 public interface ProductRepository extends JpaRepositoryImplementation<Products, Integer> {
 
 }
