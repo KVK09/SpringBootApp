c7badcbc21b202c20acba87009e2c75be973d731
K.VAMSHIKRISHNA <k.vamshikrishna2025@gmail.com>
2022-11-29 17:54:01 +0530

    add

---
+++ SpringWeb/src/main/java/com/krishna/core/ProductRestController.java
@@ -0,0 +1,20 @@
+package com.krishna.core;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductRestController {
+	
+	@Autowired
+	ProductRepository repository;
+	
+	@RequestMapping(value="/Product/")
+	public List<Products> getProducts(){
+		return repository.findAll();
+	}
+
+}
