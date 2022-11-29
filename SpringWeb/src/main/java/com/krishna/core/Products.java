af6969349039000346456119ff4014ebd93c5e31
K.VAMSHIKRISHNA <k.vamshikrishna2025@gmail.com>
2022-11-29 16:49:19 +0530

    First commit

---
+++ SpringWeb/src/main/java/com/krishna/core/Products.java
@@ -0,0 +1,34 @@
+package com.krishna.core;
+
+public class Products {
+	private int id;
+	private String name;
+	private String description;
+	private int price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+
+}
