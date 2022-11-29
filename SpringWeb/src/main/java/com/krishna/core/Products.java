702c7f027971e0b7b34c73a18ed388a75ec976a4
K.VAMSHIKRISHNA <k.vamshikrishna2025@gmail.com>
2022-11-29 17:57:12 +0530

    add

--- SpringWeb/src/main/java/com/krishna/core/Products.java
+++ SpringWeb/src/main/java/com/krishna/core/Products.java
@@ -1,9 +1,12 @@
 package com.krishna.core;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 public class Products {
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String description;
