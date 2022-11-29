ea69cf31fa20ffd79a958992ae94a43cc9567f71
K.VAMSHIKRISHNA <k.vamshikrishna2025@gmail.com>
2022-11-29 20:51:17 +0530

    Add properties

--- SpringWeb/src/main/java/com/krishna/core/Products.java
+++ SpringWeb/src/main/java/com/krishna/core/Products.java
@@ -1,9 +1,11 @@
 package com.krishna.core;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Products {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
