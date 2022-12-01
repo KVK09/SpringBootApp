9f2292818bf9a16ad517d33d4321e43b0a69dc9a
K.VAMSHIKRISHNA <k.vamshikrishna2025@gmail.com>
2022-12-02 00:20:43 +0530

    Testing app

--- SpringWeb/src/test/java/com/krishna/core/SpringWebApplicationTests.java
+++ SpringWeb/src/test/java/com/krishna/core/SpringWebApplicationTests.java
@@ -1,13 +1,22 @@
 package com.krishna.core;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class SpringWebApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testGetProducts() {
+		RestTemplate template = new RestTemplate();
+		Products product = template.getForObject("http://localhost:8080/productapi/products/1", Products.class);
+		assertNotNull(product);
+		assertEquals("Iphone",product.getName());
 	}
 
 }
